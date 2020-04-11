c13217c5714d7ee4ac15ec3e0c1a9f2b13bffef5
Mallory LeeWong <m.c.leewong@gmail.com>
2020-04-11 17:15:39 -0400

    refactor: configure ApolloClient

--- src/index.js
+++ src/index.js
@@ -4,10 +4,30 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+// import apollo dependencies from installed packages
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// create httpLink that connects ApolloClient instance with  GraphQL API
+// your GraphQL server will run on localhost 4000
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+// create ApolloClient instance by passing the httpLinka nd new instance of InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// render root App component, which is wrapped in ApolloProvider higher order component
+// receives client as prop
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
