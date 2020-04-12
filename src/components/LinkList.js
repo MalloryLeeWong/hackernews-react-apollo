052b84682199bb7575ebd883a25bb7525e73af10
Mallory LeeWong <m.c.leewong@gmail.com>
2020-04-11 21:09:49 -0400

    refactor: create list link components and set up for GraphQL query

---
+++ src/components/LinkList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Link from './Link';
+
+class LinkList extends Component {
+  // using local mock data for now
+  render() {
+    const linksToRender = [
+      {
+        id: '1',
+        description: 'Prisma turns your database into a GraphQL API',
+        url: 'https://www.prismagraphql.com',
+      },
+      {
+        id: '2',
+        description: 'The best GraphQL client',
+        url: 'https://www.apollographql.com/docs/react/',
+      },
+    ];
+
+    return (
+      <div>
+        {linksToRender.map((link) => (
+          <Link key={link.id} link={link} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default LinkList;
