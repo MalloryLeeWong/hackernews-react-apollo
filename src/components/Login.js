b87b15aba5a1e890974c337c26f13d0adbf16f5e
Mallory LeeWong <m.c.leewong@gmail.com>
2020-04-14 12:12:35 -0400

    refactor: add authentication Mutation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,25 @@
 import React, { Component } from 'react';
 import { AUTH_TOKEN } from '../constants';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+// mutations for signing in and loging in
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 class Login extends Component {
   state = {
@@ -37,22 +57,34 @@ class Login extends Component {
           />
         </div>
         <div className="flex mt3">
-          <div className="pointer mr2 button" onClick={() => this._confirm()}>
-            {login ? 'login' : 'create account'}
-          </div>
-          <div
-            className="pointer button"
-            onClick={() => this.setState({ login: !login })}
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION} // mutation prop
+            variables={{ email, password, name }} // via variables prop, GraphQL mutations receive the email, password, and name state values
+            onCompleted={(data) => this._confirm(data)} //onCompleted prop
           >
-            {login ? 'need to create an account?' : 'already have an account?'}
-          </div>
+            {(mutation) => (
+              // mutation is triggered by the onClick event
+              <div className="pointer mr2 button" onClick={mutation}>
+                {login ? 'login' : 'create account'}
+              </div>
+            )}
+          </Mutation>
+        </div>
+        <div
+          className="pointer button"
+          onClick={() => this.setState({ login: !login })}
+        >
+          {login ? 'need to create an account?' : 'already have an account?'}
         </div>
       </div>
     );
   }
 
-  _confirm = async () => {
-    // ... you'll implement this 🔜
+  // confirm method will be used to implement mutations we need to send for login
+  _confirm = async (data) => {
+    const { token } = this.state.login ? data.login : data.signup;
+    this._saveUserData(token);
+    this.props.history.push(`/`);
   };
 
   _saveUserData = (token) => {
